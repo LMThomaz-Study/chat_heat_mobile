0d50f8a86603610b784c45c914f8eea48894796a
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-10-24 16:55:09 -0300

    component send message

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
-import { SignInBox } from '../../components/SignInBox';
+import { SendMessageForm } from '../../components/SendMessageForm';
 import { styles } from './styles';
 
 export function Home() {
@@ -10,7 +10,7 @@ export function Home() {
     <View style={styles.container}>
       <Header />
       <MessageList />
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 }
